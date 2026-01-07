6bd1f19251415d2a29dfcb2b3e0573b34de5338b
Basana Venkata Karthik <140370100+basanakarthik@users.noreply.github.com>
2026-01-07 21:29:06 +0530

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,26 @@
+import { Kalam } from "next/font/google";
+import "./globals.css";
+import BackgroundMusic from "../components/BackgroundMusic"
+
+const kalam = Kalam({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+
+export const metadata = {
+  title: "Happy Birthday!",
+  description: "Celebrate your day with joy and happiness!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${kalam.className} antialiased bg-black select-none`}
+      >
+        {children}
+        <BackgroundMusic />
+      </body>
+    </html>
+  );
+}
