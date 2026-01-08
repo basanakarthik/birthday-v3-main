90d4378434a7703d10b1255c323e6f89d6e02707
basanakarthik <basanavenkatkarthik2002@gmail.com>
2026-01-08 09:48:07 +0530

    Fix image filename casing and update image paths

--- birthday-v3-main/src/components/screens/PhotosScreen.jsx
+++ birthday-v3-main/src/components/screens/PhotosScreen.jsx
@@ -13,10 +13,10 @@ export default function PhotosScreen({ onNext }) {
   const swiperRef = useRef(null)
 
   const photos = [
-    "/images/1.JPG",
-    "/images/2.JPG",
-    "/images/3.JPG",
-    "/images/4.JPG",
+    "/images/1.jpg",
+    "/images/2.jpg",
+    "/images/3.jpg",
+    "/images/4.jpg",
   ]
 
   return (
